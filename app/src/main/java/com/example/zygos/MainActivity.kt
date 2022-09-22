8bb359abba1031154023f5f343b723165dd4d0c5
Riley Xu <riley.xu@gmail.com>
2022-09-22 11:44:04 -0400

    Add nav host and integrate with bottom nav

--- app/src/main/java/com/example/zygos/MainActivity.kt
+++ app/src/main/java/com/example/zygos/MainActivity.kt
@@ -12,6 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.zygos.components.PieChart
 import com.example.zygos.ui.theme.ZygosTheme
 
@@ -27,51 +33,132 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ZygosApp() {
     ZygosTheme {
+        val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStack?.destination
+        var currentTab = zygosTabs.find { it.route == currentDestination?.route } ?: Performance
+
         Scaffold(
-            bottomBar = { ZygosNav() },
-        ) {
-            Surface(
-                modifier = Modifier
-                    .padding(it)
-                    .fillMaxSize(),
-                color = MaterialTheme.colors.background
-            ) {
-                PieChart(
-                    values = listOf(0.2f, 0.3f, 0.4f, 0.1f),
-                    colors = listOf(
-                        Color(0xFF004940),
-                        Color(0xFF005D57),
-                        Color(0xFF04B97F),
-                        Color(0xFF37EFBA)
-                    ),
-                    modifier = Modifier.size(100.dp),
+            bottomBar = {
+                ZygosNav(
+                    tabs = zygosTabs,
+                    currentTab = currentTab.route,
+                    onTabSelected = { tab ->
+                        if (tab.route != currentTab.route) navController.navigateSingleTopTo(tab.route)
+                    },
                 )
+            },
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = Performance.route,
+                modifier = Modifier.padding(innerPadding),
+            ) {
+                composable(route = Performance.route) {
+                    PerformanceScreen(innerPadding)
+                }
+                composable(route = Holdings.route) {
+                    HoldingsScreen(innerPadding)
+                }
+                composable(route = Chart.route) {
+                    ChartScreen(innerPadding)
+                }
             }
         }
     }
 }
 
+
+
+
 @Composable
-fun ZygosNav() {
-    var selectedItem by remember { mutableStateOf(0) }
-    val items = listOf(Icons.Sharp.ShowChart, Icons.Sharp.PieChart, Icons.Sharp.CandlestickChart)
+fun PerformanceScreen(
+    innerPadding: PaddingValues,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Text("Performance Screen")
+    }
+}
+
+@Composable
+fun HoldingsScreen(
+    innerPadding: PaddingValues,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        PieChart(
+            values = listOf(0.2f, 0.3f, 0.4f, 0.1f),
+            colors = listOf(
+                Color(0xFF004940),
+                Color(0xFF005D57),
+                Color(0xFF04B97F),
+                Color(0xFF37EFBA)
+            ),
+            modifier = Modifier.size(100.dp),
+        )
+    }
+}
+
+@Composable
+fun ChartScreen(
+    innerPadding: PaddingValues,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Text("Chart Screen")
+    }
+}
+
+@Composable
+fun ZygosNav(
+    tabs: List<ZygosDestination>,
+    currentTab: String,
+    onTabSelected: (ZygosDestination) -> Unit,
+) {
     BottomNavigation(
         modifier = Modifier.height(48.dp)
         //elevation = 0.dp,
     ) {
-        items.forEachIndexed { index, item ->
+        tabs.forEach { item ->
             BottomNavigationItem(
                 icon = { Icon(
-                    item,
+                    item.icon,
                     contentDescription = null,
                     modifier = Modifier.size(30.dp)
                     // For some reason height can't make the icon larger than some fixed value
                     // (maybe to do with the fixed BottomNavigationHeight = 56.dp), but size can.
+                    // Or maybe the preview is just buggy.
                 ) },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index }
+                selected = currentTab == item.route,
+                onClick = { onTabSelected(item) },
             )
         }
+    }
+}
+
+fun NavHostController.navigateSingleTopTo(route: String) {
+    backQueue.removeIf { it.destination.route == route }
+    navigate(route) {
+//        popUpTo(
+//            this@navigateSingleTopTo.graph.findStartDestination().id
+//        ) {
+//            saveState = true
+//        }
+//        launchSingleTop = true
+        restoreState = true
     }
 }
 
---
+++ app/src/main/java/com/example/zygos/ZygosDestinations.kt
@@ -0,0 +1,31 @@
+package com.example.zygos
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.CandlestickChart
+import androidx.compose.material.icons.sharp.PieChart
+import androidx.compose.material.icons.sharp.ShowChart
+import androidx.compose.ui.graphics.vector.ImageVector
+
+interface ZygosDestination {
+    val icon: ImageVector
+    val route: String
+}
+
+// See https://fonts.google.com/icons?icon.category=Business%26Payments&icon.platform=web&icon.style=Sharp
+
+object Performance : ZygosDestination {
+    override val icon = Icons.Sharp.ShowChart
+    override val route = "performance"
+}
+
+object Holdings : ZygosDestination {
+    override val icon = Icons.Sharp.PieChart
+    override val route = "holdings"
+}
+
+object Chart : ZygosDestination {
+    override val icon = Icons.Sharp.CandlestickChart
+    override val route = "chart"
+}
+
+val zygosTabs = listOf(Performance, Holdings, Chart)
