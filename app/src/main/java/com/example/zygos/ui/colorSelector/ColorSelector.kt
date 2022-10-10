f0fd81b9ae71a1a453f2e49f10c266745e7132d2
Riley Xu <riley.xu@gmail.com>
2022-10-10 18:24:27 -0400

    Draggable color selector!

--- app/src/main/java/com/example/zygos/ui/colorSelector/ColorSelector.kt
+++ app/src/main/java/com/example/zygos/ui/colorSelector/ColorSelector.kt
@@ -1,5 +1,6 @@
 package com.example.zygos.ui.colorSelector
 
+import android.util.Log
 import android.view.MotionEvent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
@@ -12,9 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -34,7 +40,8 @@ val colors = ImmutableList(listOf(
 @Composable
 fun ColorCircle(
     brightness: Float,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    overlay: DrawScope.() -> Unit = { },
 ) {
     Canvas(modifier.aspectRatio(1f).fillMaxSize()) {
         drawCircle(brush = Brush.sweepGradient(colors.items))
@@ -46,6 +53,8 @@ fun ColorCircle(
         )
 
         drawCircle(color = Color.Black.copy(alpha = 1 - brightness))
+
+        overlay()
     }
 }
 
@@ -67,7 +76,7 @@ fun ColorSelector(
         val x = point.x - boxSize.width / 2
         val y = point.y - boxSize.height / 2
 
-        val r = clamp(sqrt(x * x + y * y), 0f, 1f)
+        val r = clamp(2 * sqrt(x * x + y * y) / minOf(boxSize.width, boxSize.height), 0f, 1f)
         var phi = Math.toDegrees(atan2(y, x).toDouble()).toFloat()
         if (phi < 0) phi += 360
         val stopIndex = phi.toInt() / 60
@@ -80,38 +89,62 @@ fun ColorSelector(
         return Color(ColorUtils.blendARGB(Color.White.toArgb(), hue, r))
     }
 
-//    var currentColor by remember { mutableStateOf(initialColor) }
+    var currentColor by remember { mutableStateOf(initialColor) }
 //    var currentHover by remember { mutableStateOf(colorToPixel(initialColor)) }
-    var currentHover by remember { mutableStateOf(Offset(0f, 0f)) }
+    var currentHover by remember(boxSize) { mutableStateOf(Offset(boxSize.width / 2f, boxSize.height / 2f)) }
+    var dragStart by remember { mutableStateOf(currentHover) }
     var brightness by remember { mutableStateOf(1.0f) }
 
+    val indicatorRadius = with(LocalDensity.current) { 10.dp.toPx() }
+    val indicatorWidth = with(LocalDensity.current) { 5.dp.toPx() }
+    val indicatorColor = remember { derivedStateOf {
+        if (brightness > 0.5) Color.Black else Color.White
+    } }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-        ColorCircle(
-            brightness = brightness,
+
+        Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
+                .aspectRatio(1f)
                 .fillMaxWidth()
                 .onGloballyPositioned {
                     boxSize = it.size
-                    currentHover = Offset(boxSize.width / 2f, boxSize.height / 2f)
                 }
                 .pointerInteropFilter(
 //                requestDisallowInterceptTouchEvent = disallowIntercept
-            ) { motionEvent ->
-                if (motionEvent.action == MotionEvent.ACTION_MOVE ||
-                    motionEvent.action == MotionEvent.ACTION_DOWN
-                ) {
+                ) { motionEvent ->
+                    if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                        dragStart = Offset(motionEvent.x, motionEvent.y)
+                    } else if (motionEvent.action == MotionEvent.ACTION_MOVE) {
 //                    disallowIntercept(true)
-                    currentHover = Offset(motionEvent.x, motionEvent.y)
-                } else {
+                        currentHover += Offset(motionEvent.x, motionEvent.y) - dragStart
+                        dragStart = Offset(motionEvent.x, motionEvent.y)
+                        currentColor = pixelToColor(currentHover)
+                    } else {
 //                    disallowIntercept(false)
+                    }
+                    true
                 }
-                true
+        ) {
+            ColorCircle(brightness = brightness)
+            Canvas(Modifier.fillMaxSize()) {
+                drawCircle(
+                    color = currentColor,
+                    radius = indicatorRadius,
+                    center = currentHover,
+                )
+                drawCircle(
+                    color = indicatorColor.value,
+                    radius = indicatorRadius,
+                    center = currentHover,
+                    style = Stroke(indicatorWidth),
+                )
             }
-        )
+        }
 
         Text("${pixelToColor(currentHover)}", Modifier.padding(start = 20.dp, top = 50.dp))
     }
